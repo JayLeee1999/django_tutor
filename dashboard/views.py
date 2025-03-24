a0f845fe9697d74342828e43cff6f9a64f3a6908
JayLeee1999 <jungjun4@gmail.com>
2025-03-24 15:50:19 +0900

    데이터 모델 만들고, 테이블 데이터 차트에 랜더링 구현

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,4 +1,14 @@
 from django.shortcuts import render
 
+from .models import CountryData
+
 def dashboard(request):
-    return render(request, 'dashboard.html')
+
+    datas = CountryData.objects.all()
+    context = {
+        'dataset' : datas
+    }
+    return render(request, 'dashboard.html', context)
+# DB 테이블에서 데이터 조회
+# 비즈니스 로직
+# 랜더링 데이터 만들기
